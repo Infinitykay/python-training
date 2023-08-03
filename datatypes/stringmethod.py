666257b32ffb2bf2d9b6344433b31d749cd48e93
Infinitykay <bekay.asa@icloud.com>
2023-08-03 19:35:09 +0800

    stringslicing

---
+++ datatypes/stringmethod.py
@@ -0,0 +1,20 @@
+# built in strings method
+
+# ord, len, chr, str
+
+#  we get the character value through chr() method
+print(chr(65))
+
+#  we get the unicode value through ord() method
+print(ord("A"))
+
+no1= 30 # now "30"
+no2 = 65
+no3 = "43"
+no4 = "68"
+no5 = "77"
+
+total = str(no1) + str(no2)
+total1 = int(no3) + (no1)
+print(total1)
+print(no2+no3)
---
+++ datatypes/stringslicing.py
@@ -0,0 +1,14 @@
+sentence1 = "Enter username here"
+sentence2 = "post a status now"
+
+# counting starts from 0
+# when slicing we use []
+# we have a start and an end in our slice
+first_word = sentence2[5:6]
+second_word = sentence2[7:13]
+last_word = sentence2[-3:]
+
+
+print(first_word)
+print(second_word)
+print(last_word)
