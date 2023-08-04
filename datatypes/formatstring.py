1997c9eb9984be579e9dd3a8c9ded5ad18197b01
Infinitykay <bekay.asa@icloud.com>
2023-08-04 20:29:57 +0800

    input,format strings and in operator

---
+++ datatypes/formatstring.py
@@ -0,0 +1,33 @@
+name = "Paul Obiahu"
+
+item = "Apple"
+
+# print(name[0:5])
+
+# Reversing a string
+# print(item[::-1])
+
+
+# strings concatentaion
+
+firstName = "Bukunmi"
+lastName = "Asaolu"
+
+# format string
+# sentence = "I am " + " " + firstName + " " + lastName
+sentence = f"I am {firstName} {lastName}"
+
+
+# print(sentence)
+
+myname = "Bekay"
+colour = "red"
+club = "Arsenal"
+food = "amala"
+
+statement = f"Hi, I'm {myname}. I love color {colour}. I am an {club} fan."
+
+print(statement)
+
+statement2 = f"I'm {myname}. I love {club} and {food}"
+print(statement2)
---
+++ datatypes/strexercise.py
@@ -0,0 +1,21 @@
+# input function in python
+
+# username = input("What is your username? ")
+
+# take user first name, last name, school, favorite color and favorite sport
+
+# Sample 
+
+# Hello Bello Abdulhakeem. you schooled at BSU. your favorite color is blue and you are a football fan.
+
+firstName = input("What is your firstname?")
+lastName = input("What is your lastname?")
+school = input("What school did you attend?")
+favoriteColor = input("What is your favorite color?")
+footballClub = input("What is your football club?")
+
+print(f'Hello {firstName.title()} {lastName.title()
+}. You schooled at {school.upper()}, your favorite color is {favoriteColor.capitalize()} and you are a {footballClub} fan')
+
+# Your name is ola
+
